693cb56b91228b01e7757790e0cca6de7357cfa0
Mansi Saxena <saxenamansi346@gmail.com>
2025-12-28 14:38:16 +0530

    initial location

---
+++ ES/es3.js
@@ -0,0 +1,49 @@
+//try catch
+// let x = 5;
+
+// try {
+//   x = y + 1;
+// } catch(err) {
+//   let text = err.name;
+//   console.log(text)
+// }
+
+
+
+//switch
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
+// console.log(day);
+
+//do...while
+let text = ""
+let i = 0;
+
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10){
+  console.log(text);
+  '<br>'
+};  
